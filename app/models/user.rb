85cb906f515cff92bb16d295f3389f874ebe3253
tyler21s <tyler@relationshipapp.com>
2021-08-13 19:46:05 +0800

    update user status constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
    validates :name, length: { maximum: 255 },presence: true
    validates :password, length: { maximum: 255 },presence: true
    validates :email, length: { maximum: 255 },presence: true
+   validates :status, format: { with: /^[0|1]*$/,message: "only allows letters" ,:multiline => true}
 end

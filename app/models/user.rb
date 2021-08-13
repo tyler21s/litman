73e12997189f440f6bcf7c598db665786957af48
tyler21s <tyler@relationshipapp.com>
2021-08-13 19:27:05 +0800

    add constraint for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+   validates :name, length: { maximum: 255 },presence: true
+   validates :password, length: { maximum: 255 },presence: true
+   validates :email, length: { maximum: 255 },presence: true
 end

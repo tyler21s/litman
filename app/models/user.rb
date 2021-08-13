a3151a079bd865b31291b40b9248e6c99601fc51
tyler21s <tyler@relationshipapp.com>
2021-08-13 20:00:25 +0800

    add select to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
    validates :name, length: { maximum: 255 },presence: true
    validates :password, length: { maximum: 255 },presence: true
    validates :email, length: { maximum: 255 },presence: true
-   validates :status, format: { with: /^[0|1]*$/,message: "only allows letters" ,:multiline => true}
+   # validates :status, format: { with: /^[0|1]*$/,message: "only allows letters" ,:multiline => true}
+   enum status: [ :active, :inactive ]
+   validates :status, inclusion: { in: %w(active inactive),
+    message: "%{value} is not a valid size" }
 end

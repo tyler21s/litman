a22e0a6f907c2cab0689ac10d555ee35264f850c
tyler21s <tyler@relationshipapp.com>
2021-08-14 17:04:51 +0800

    create api

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,10 @@
 class Animal < ApplicationRecord
+  def self.fetch
+    require 'httparty'
+    @response = HTTParty.get("https://axoltlapi.herokuapp.com/")
+    @response = JSON.parse(@response.body) 
+    Animal.create(url: @response['url'] ,facts: @response['facts'],pics_repo:@response['pics_repo'] ,api_repo: @response['api_repo'])
+  end
 end
+
+

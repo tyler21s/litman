98d9d50845f3f3205fc6c3b574bfac6ad18c7ac2
tyler21s <tyler@relationshipapp.com>
2021-08-14 18:19:41 +0800

    add Instance method push_remo

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,13 +3,19 @@ class Animal < ApplicationRecord
     require 'httparty'
     @response = HTTParty.get("https://axoltlapi.herokuapp.com/")
     @response = JSON.parse(@response.body) 
-    Animal.create(url: @response['url'] ,facts: @response['facts'],pics_repo:@response['pics_repo'] ,api_repo: @response['api_repo'])
+    Animal.create(url: @response['url'] ,facts: @response['facts'],pics_repo:@response['pics_repo'] ,api_repo: @response['api_repo']) 
   end
-  def self.push_remote
-    require 'httparty'
-    @response = HTTParty.get("https://axoltlapi.herokuapp.com/")
-    @response = JSON.parse(@response.body)
-    result = HTTParty.post('http://requestbin.net/r/6cy069o6',@response)
+
+  def push_remote
+    body = {
+      body:{
+        url: url,
+        facts:  facts,
+        pics_repo:pics_repo,
+        api_repo: api_repo
+      }
+    }
+    HTTParty.post("http://requestbin.net/r/8jsviqft",body)
   end
 end
 

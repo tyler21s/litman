22e48454d2ee8d7e48bc98d2bc374e2650641e06
tyler21s <tyler@relationshipapp.com>
2021-08-14 17:25:27 +0800

    add push_romte api

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,6 +5,11 @@ class Animal < ApplicationRecord
     @response = JSON.parse(@response.body) 
     Animal.create(url: @response['url'] ,facts: @response['facts'],pics_repo:@response['pics_repo'] ,api_repo: @response['api_repo'])
   end
+  def self.push_remote
+    require 'httparty'
+    @response = HTTParty.get("https://axoltlapi.herokuapp.com/")
+    result = HTTParty.post('http://requestbin.net/r/6cy069o6',@response)
+  end
 end
 
 
